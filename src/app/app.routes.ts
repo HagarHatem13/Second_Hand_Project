1115a1b9a078b1b98fd6ee3eaf78a3ea9d1ca7c1
HagarHatem13 <hagarhatem901@gmail.com>
2026-05-08 05:29:25 +0300

    Create app.routes.ts

---
+++ src/app/app.routes.ts
@@ -0,0 +1,59 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./pages/signup/signup.component').then(m => m.SignupComponent)
+  },
+  {
+    path: 'item/:id',
+    loadComponent: () => import('./pages/item-details/item-details.component').then(m => m.ItemDetailsComponent)
+  },
+  {
+    path: 'create-listing',
+    loadComponent: () => import('./pages/create-listing/create-listing.component').then(m => m.CreateListingComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'analytics',
+    loadComponent: () => import('./pages/analytics/analytics.component').then(m => m.AnalyticsComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'listings',
+    loadComponent: () => import('./pages/listings/listing.component').then(m => m.ListingComponent)
+  },
+  {
+    path: 'wishlist',
+    loadComponent: () => import('./pages/wishlist/wishlist.component').then(m => m.WishlistComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'messages',
+    loadComponent: () => import('./pages/messages/message.component').then(m => m.MessageComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
